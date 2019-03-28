6281e2588b0633eeeef22ed5a849649f5cdcff74
jeffrey lantinga <jeffreylantinga@hotmail.com>
2019-03-28 14:10:47 +0100

    charts

--- src/app/data/data.component.ts
+++ src/app/data/data.component.ts
@@ -38,7 +38,7 @@ export class DataComponent implements OnInit {
       ]
     },
     options: {
-      
+
       scales: {
         xAxes: [{
           display: true
@@ -77,72 +77,106 @@ export class DataComponent implements OnInit {
   }
 
   OnClickMe() {
+    this.chart = new Chart('canvas', {
+      type: 'line',
+      data: {
+        labels: this.alldates,
+        datasets: [
+          {
+            label: "Gebruikt",
+            data: this.gebruikt,
+            borderColor: "#3cba0f",
+            fill: false
+          },
+          {
+            label: "Opgeleverd",
+            data: this.opgeleverd,
+            borderColor: "#ffcc00",
+            fill: false
+          },
+        ]
+      },
+      options: {
 
-  }
+        scales: {
+          xAxes: [{
+            display: true
+          }],
+          yAxes: [{
+            display: true
+          }],
+        },
+        elements: {
+          point: {
+            radius: 1
+          }
+        }
+      }});
+    }
 
   getLatest() {  //TODO: add right API call when API is up to date
-    this.httpClient.get<Object[]>(this.apiURL + '/energie').subscribe((data => {
-      this.gaugeValue = data[data.length - 1]["verbruik"];
-      this.opleverValue = data[data.length - 1]["opgeleverd"];
-    }));
-  }
+        this.httpClient.get<Object[]>(this.apiURL + '/energie').subscribe((data => {
+          this.gaugeValue = data[data.length - 1]["verbruik"];
+          this.opleverValue = data[data.length - 1]["opgeleverd"];
+        }));
+      }
 
   getDate() {
-    this.httpClient.get<object[]>(this.apiURL + '/energie').subscribe((data => {
-      this.sorting(data);
-    }));
+        this.httpClient.get<object[]>(this.apiURL + '/energie').subscribe((data => {
+          this.sorting(data);
+        }));
 
-  }
+      }
 
   sorting(data) {
-    this.gebruikt = [];
-    this.opgeleverd = [];
-    this.alldates = [];
-    let date = new Date(data[0]["tijdstip"]);
-    this.alldates.push(this.dateformatter.dateFormat(date));
-    let tempData = [];
-    console.log(data);
+        this.gebruikt = [];
+        this.opgeleverd = [];
+        this.alldates = [];
+        let date = new Date(data[0]["tijdstip"]);
+        this.alldates.push(this.dateformatter.dateFormat(date));
+        let tempData =[];
+        console.log(data);
 
-    data.forEach((dat, index, array) => {
-      
-      let tempDate = new Date(dat["tijdstip"]);
-      if (this.sortingTime(tempDate, date) && index !== array.length - 1) {
-        tempData.push(dat);
-      } else if(!this.alldates.includes(this.dateformatter.dateFormat(tempDate)))  {
-        if (index === array.length -1) console.log(dat);
-        if (this.alldates.length === 1 ) console.log(dat);
+        data.forEach((dat, index, array) => {
 
-        let tempArray = [];
-        tempArray.push((tempData.map(dat => dat["verbruik"])));
-        this.gebruikt.push(Math.round((tempArray["0"].reduce((old, add) => old + add, 0)) / tempArray["0"].length));
+          let tempDate = new Date(dat["tijdstip"]);
+          if (this.sortingTime(tempDate, date) && index !== array.length - 1) {
+            tempData.push(dat);
+          } else if (!this.alldates.includes(this.dateformatter.dateFormat(tempDate))) {
+            if (index === array.length - 1) console.log(dat);
+            if (this.alldates.length === 1) console.log(dat);
 
-        tempArray = [];
-        tempArray.push((tempData.map(dat => dat["opgeleverd"])));
-        this.opgeleverd.push(Math.round((tempArray["0"].reduce((old, add) => old + add, 0)) / tempArray["0"].length));
+            let tempArray = [];
+            tempArray.push((tempData.map(dat => dat["verbruik"])));
+            this.gebruikt.push(Math.round((tempArray["0"].reduce((old, add) => old + add, 0)) / tempArray["0"].length));
 
-        tempData = [];
-        tempArray = [];
-        tempData.push(dat);
-        if(index !== array.length -1) this.alldates.push(this.dateformatter.dateFormat(tempDate));
-        date = tempDate;
-      }
-    });
-    
+            tempArray = [];
+            tempArray.push((tempData.map(dat => dat["opgeleverd"])));
+            this.opgeleverd.push(Math.round((tempArray["0"].reduce((old, add) => old + add, 0)) / tempArray["0"].length));
 
-    this.chart.data.datasets["0"].data = this.gebruikt;
-    this.chart.data.datasets["1"].data = this.opgeleverd;
-    this.chart.data.labels =  this.alldates;
+            tempData = [];
+            tempArray = [];
+            tempData.push(dat);
+            if (index !== array.length - 1) this.alldates.push(this.dateformatter.dateFormat(tempDate));
+            date = tempDate;
+          }
+        });
 
-    console.log(this.chart.data.datasets["0"].data);
-    console.log(this.chart.data.datasets["1"].data);
-    console.log(this.chart.data.labels)
+
+        this.chart.data.datasets["0"].data = this.gebruikt;
+        this.chart.data.datasets["1"].data = this.opgeleverd;
+        this.chart.data.labels = this.alldates;
+
+        console.log(this.chart.data.datasets["0"].data);
+        console.log(this.chart.data.datasets["1"].data);
+        console.log(this.chart.data.labels)
 
 
     this.chart.update();
-  }
+      }
 
   sortingTime(originalDate, compareDate) {
-    if (this.tab === "Minuut") {
+        if(this.tab === "Minuut") {
       return originalDate.getMinutes() === compareDate.getMinutes();
     } else if (this.tab === "Uur") {
       return originalDate.getHours() === compareDate.getHours();
